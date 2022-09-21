6421f75c898bf3b74fe81c5b5ebf2f1cb3e593ee
al-jabir <aljabir098@gmail.com>
2022-09-21 12:22:05 +0600

    i have finish navbar and style with responsive

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <div className="navbar">
-      <div className="leftside">
-        <img className="logo" src={logo} alt="logo" />
+      <div className="leftside" id={showLinks ? "open" : "close"}>
+        <img src={logo} alt="logo" />
+        <div className="hiddenLinks">
+          <Link to="/">home</Link>
+          <Link to="/menu">menu</Link>
+          <Link to="/about">about</Link>
+          <Link to="/contact">contact</Link>
+        </div>
       </div>
       <div className="rightside">
         <Link to="/">home</Link>
-
         <Link to="/menu">menu</Link>
         <Link to="/about">about</Link>
         <Link to="/contact">contact</Link>
+        <button onClick={() => setShowLinks(!showLinks)}>
+          <MenuIcon style={{ color: "black" }} />
+        </button>
       </div>
       <Outlet />
     </div>
