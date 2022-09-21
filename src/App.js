6584731180b7d64d9ad8d02111cd7788c6b23c31
al-jabir <aljabir098@gmail.com>
2022-09-21 11:30:26 +0600

    Add navbar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navigation/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
